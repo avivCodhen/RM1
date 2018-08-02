dbf8c5e654013b3795f8a3da80d2cc3b8cd6a52c
yarink <yarink@liveperson.com>
2018-08-02 16:25:21 +0300

    start working on client to server

--- SavingData/app/src/main/java/com/strongest/savingdata/Activities/MainActivity.java
+++ SavingData/app/src/main/java/com/strongest/savingdata/Activities/MainActivity.java
@@ -4,8 +4,15 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.strongest.savingdata.R;
+import com.strongest.savingdata.Server.ApiClient;
+import com.strongest.savingdata.Server.ApiInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends FragmentActivity {
     private static int SPLASH_SCREEN = 3000;
@@ -16,7 +23,21 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        ApiInterface apiInterface = ApiClient.getApiClientTmp().create(ApiInterface.class);
+        Call<String> call = apiInterface.getTmp();
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
 
+                Log.d("aviv", "onResponse: "+  response.body());
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d("aviv", "onFailure: "+ t.toString()+" ");
+            }
+        });
+        if(true) return;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
--- SavingData/app/src/main/java/com/strongest/savingdata/Server/ApiClient.java
+++ SavingData/app/src/main/java/com/strongest/savingdata/Server/ApiClient.java
@@ -1,7 +1,14 @@
 package com.strongest.savingdata.Server;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -12,10 +19,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     public static final String BASE_URL = "http://10.0.2.2:80/site/_db/";
-    //public static final String BASE_URL = "http://159.65.207.28/_db/";
+    public static final String BASE_SERVER_URL = "http://11.0.1.17:8080/";
     //public static final String BASE_URL = "http://localhost/site/_db/";
 
     public static Retrofit retrofit = null;
+    public static Retrofit retrofitServerApi = null;
 
     public static Retrofit getApiClient(){
 
@@ -28,6 +36,31 @@ public class ApiClient {
                     build();
         }
         return retrofit;
+    }
+    private static String token = "eyJraWQiOiI0bjF2WG9jR3dYTmc2TVJzSmQ3RHlXS042M1l4K2NpYllFNFRTNGIzWEtZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmMGJjNzJjMi02Mjc2LTQ1MjUtOWY3Mi05OGE0MjMyZTYzMWEiLCJhdWQiOiIzNGdqNWNlMW5la2UzZnMwZTBybmlxcmhlNiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJldmVudF9pZCI6IjM5OGRkZjMzLTk2NGMtMTFlOC05NDVkLTI1MDBkNjNmMWFkZCIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNTMzMjExNDc2LCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9PbVpzSmRtejgiLCJjb2duaXRvOnVzZXJuYW1lIjoiZm9vYmFyeWFyaW4iLCJleHAiOjE1MzMyMTg4MTMsImlhdCI6MTUzMzIxNTIxMywiZW1haWwiOiJ1c3VzdWV1QGRoZGguY29tIn0.MDpTJIt189Kwd3VwCyOdsQkR2OTmqvCLr8B9XX8RoYlcBCLBhcElbB_diw5yJpFzC12uPfGW-z5vFfef1RQzwHQNgFC5xP9KscancomHyYpbh1-L-VNPMy9BJgo5M3Rk7vYxLhw02naPQPB2YZw7sRzDSSzrmub2ECF3qtlJ4ugWYTaKzwsk-ByZWI5RrnDWaKs47uoRIuTryDVlsW0HYhzB2KLS4h3kqBoj0H4ypTVTN8IpMAYoD3TyWI8YiSC1h54Q0tB38_7JD9L3t8Z7Snl0-U3fxBbDT4vmgowZONw96JqsCbklurKEHqnglyuAsRucOlydVY7GJUjCxq2BEw";
+
+    public static Retrofit getApiClientTmp(){
+        if(retrofitServerApi == null) {
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request newRequest = chain.request().newBuilder()
+                            .addHeader("Authorization", "Bearer " + token)
+                            .build();
+                    return chain.proceed(newRequest);
+                }
+            }).build();
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+            retrofitServerApi = new Retrofit.Builder()
+                    .client(client)
+                    .baseUrl(BASE_SERVER_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofitServerApi;
+
     }
 
 
